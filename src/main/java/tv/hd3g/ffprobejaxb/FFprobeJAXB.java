deea3b7d816ae86d83cdc984695825233df58f00
hdsdi3g <admin@hd3g.tv>
2020-06-25 18:38:23 +0200

    Simple and automatic code clean

--- src/main/java/tv/hd3g/ffprobejaxb/FFprobeJAXB.java
+++ src/main/java/tv/hd3g/ffprobejaxb/FFprobeJAXB.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of ffprobe-jaxb.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
@@ -8,12 +8,12 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * Copyright (C) hdsdi3g for hd3g.tv 2018
  * 
-*/
+ */
 package tv.hd3g.ffprobejaxb;
 
 import java.io.ByteArrayInputStream;
@@ -40,76 +40,77 @@ import org.ffmpeg.ffprobe.ChapterType;
 import org.ffmpeg.ffprobe.ErrorType;
 import org.ffmpeg.ffprobe.FfprobeType;
 import org.ffmpeg.ffprobe.FormatType;
-import org.ffmpeg.ffprobe.FrameType;
 import org.ffmpeg.ffprobe.LibraryVersionType;
 import org.ffmpeg.ffprobe.PacketType;
 import org.ffmpeg.ffprobe.PixelFormatType;
 import org.ffmpeg.ffprobe.ProgramType;
 import org.ffmpeg.ffprobe.ProgramVersionType;
 import org.ffmpeg.ffprobe.StreamType;
-import org.ffmpeg.ffprobe.SubtitleType;
 import org.xml.sax.SAXException;
 
 public class FFprobeJAXB {
-	
+
 	public final FfprobeType probe_result;
-	
-	public FFprobeJAXB(String xml_content, Consumer<String> onWarnLog) throws IOException {
+
+	public FFprobeJAXB(final String xml_content, final Consumer<String> onWarnLog) throws IOException {
 		try {
-			JAXBContext jc = JAXBContext.newInstance("org.ffmpeg.ffprobe");
-			Unmarshaller unmarshaller = jc.createUnmarshaller();
+			final JAXBContext jc = JAXBContext.newInstance("org.ffmpeg.ffprobe");
+			final Unmarshaller unmarshaller = jc.createUnmarshaller();
 			// prepare an error catcher if trouble are catched during import.
 			unmarshaller.setEventHandler((ValidationEventHandler) e -> {
-				ValidationEventLocator localtor = e.getLocator();
-				onWarnLog.accept("XML validation: " + e.getMessage() + " [s" + e.getSeverity() + "] at line " + localtor.getLineNumber() + ", column " + localtor.getColumnNumber() + " offset " + localtor.getOffset() + " node: " + localtor.getNode() + ", object " + localtor.getObject());
+				final ValidationEventLocator localtor = e.getLocator();
+				onWarnLog.accept("XML validation: " + e.getMessage() + " [s" + e.getSeverity() + "] at line " + localtor
+				        .getLineNumber() + ", column " + localtor.getColumnNumber() + " offset " + localtor.getOffset()
+				                 + " node: " + localtor.getNode() + ", object " + localtor.getObject());
 				return true;
 			});
-			
-			DocumentBuilderFactory xmlDocumentBuilderFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder xmlDocumentBuilder = xmlDocumentBuilderFactory.newDocumentBuilder();
+
+			final DocumentBuilderFactory xmlDocumentBuilderFactory = DocumentBuilderFactory.newInstance();
+			final DocumentBuilder xmlDocumentBuilder = xmlDocumentBuilderFactory.newDocumentBuilder();
 			xmlDocumentBuilder.setErrorHandler(null);
-			
-			Document document = xmlDocumentBuilder.parse(new ByteArrayInputStream(xml_content.getBytes(StandardCharsets.UTF_8)));
-			
-			JAXBElement<FfprobeType> result = unmarshaller.unmarshal(document, FfprobeType.class);
+
+			final Document document = xmlDocumentBuilder.parse(new ByteArrayInputStream(xml_content.getBytes(
+			        StandardCharsets.UTF_8)));
+
+			final JAXBElement<FfprobeType> result = unmarshaller.unmarshal(document, FfprobeType.class);
 			probe_result = result.getValue();
 		} catch (JAXBException | SAXException | ParserConfigurationException e1) {
 			throw new IOException("Can't load XML content", e1);
 		}
 	}
-	
+
 	public List<ChapterType> getChapters() {
 		return probe_result.getChapters().getChapter();
 	}
-	
+
 	public List<StreamType> getStreams() {
 		return probe_result.getStreams().getStream();
 	}
-	
+
 	public FormatType getFormat() {
 		return probe_result.getFormat();
 	}
-	
+
 	public ErrorType getError() {
 		return probe_result.getError();
 	}
-	
+
 	public ProgramVersionType getProgramVersion() {
 		return probe_result.getProgramVersion();
 	}
-	
+
 	public List<LibraryVersionType> getLibraryVersions() {
 		return probe_result.getLibraryVersions().getLibraryVersion();
 	}
-	
+
 	public List<PixelFormatType> getPixelFormats() {
 		return probe_result.getPixelFormats().getPixelFormat();
 	}
-	
+
 	public List<PacketType> getPackets() {
 		return probe_result.getPackets().getPacket();
 	}
-	
+
 	/**
 	 * {@link FrameType }
 	 * {@link SubtitleType }
@@ -117,7 +118,7 @@ public class FFprobeJAXB {
 	public List<Object> getFrames() {
 		return probe_result.getFrames().getFrameOrSubtitle();
 	}
-	
+
 	/**
 	 * {@link PacketType }
 	 * {@link FrameType }
@@ -126,21 +127,24 @@ public class FFprobeJAXB {
 	public List<Object> getPacketsAndFrames() {
 		return probe_result.getPacketsAndFrames().getPacketOrFrameOrSubtitle();
 	}
-	
+
 	public List<ProgramType> getPrograms() {
 		return probe_result.getPrograms().getProgram();
 	}
-	
-	public static final Predicate<StreamType> filterVideoStream = stream_type -> stream_type.getCodecType().equals("video");
-	public static final Predicate<StreamType> filterAudioStream = stream_type -> stream_type.getCodecType().equals("audio");
-	public static final Predicate<StreamType> filterDataStream = stream_type -> stream_type.getCodecType().equals("data");
-	
+
+	public static final Predicate<StreamType> filterVideoStream = stream_type -> stream_type.getCodecType().equals(
+	        "video");
+	public static final Predicate<StreamType> filterAudioStream = stream_type -> stream_type.getCodecType().equals(
+	        "audio");
+	public static final Predicate<StreamType> filterDataStream = stream_type -> stream_type.getCodecType().equals(
+	        "data");
+
 	public Stream<StreamType> getVideoStreams() {
 		return getStreams().stream().filter(filterVideoStream);
 	}
-	
+
 	public Stream<StreamType> getAudiosStreams() {
 		return getStreams().stream().filter(filterAudioStream);
 	}
-	
+
 }
